0afd990c6d240c39705ae62f8d8e0ffd4a9f778f
Zheng Cao <chori84@gmail.com>
2018-02-06 23:05:25 -0800

    Finish basic version of the program

---
+++ calculator.rb
@@ -0,0 +1,51 @@
+# List of operations supported by the program
+operations = ["add", "+", "subtract", "-", "multiply", "*", "divide", "/"]
+
+# Welcome message and instruction
+puts "Welcome to the calculator program. What operation do you want to do? Please choose one from the following list: "
+puts operations
+
+# Collect operation choice from user
+user_operation = gets.chomp
+until operations.include?(user_operation)
+  puts "Please make a valid choice: "
+  user_operation = gets.chomp
+end
+
+# Collect first number from user
+print "Please provide the first integer: "
+first_integer_string = gets.chomp
+first_integer = first_integer_string.to_i
+until first_integer.to_s == first_integer_string
+  print "This isn't an integer. Please provide a valid integer: "
+  first_integer_string = gets.chomp
+  first_integer = first_integer_string.to_i
+end
+
+# Collect second number from user
+print "Please provide the second integer: "
+second_integer_string = gets.chomp
+second_integer = second_integer_string.to_i
+until second_integer.to_s == second_integer_string
+  print "This isn't an integer. Please provide a valid integer: "
+  second_integer_string = gets.chomp
+  second_integer = second_integer_string.to_i
+end
+
+# Calculate and output result
+puts "Here is the result: "
+case user_operation
+  when "add", "+"
+    puts first_integer + second_integer
+  when "subtract", "-"
+    puts first_integer - second_integer
+  when "multiply", "*"
+    puts first_integer * second_integer
+  when
+    if second_integer == 0
+      puts "For division, the second integer cannot be 0."
+    else
+      puts first_integer / second_integer
+    end
+end
+puts "Thank you for using the Calculator program."
