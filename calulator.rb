a64a8f42d15754c0b5645e2f415435454e131a31
Kate Pond <kate@thepondsedge.com>
2018-02-07 09:08:13 -0800

    creates the first version of the program

---
+++ calulator.rb
@@ -0,0 +1,80 @@
+def add(num1,num2)
+  return num1 + num2
+end
+
+def subtract(num1,num2)
+  return num1 - num2
+end
+
+def multiply(num1,num2)
+  return num1 * num2
+end
+
+def divide(num1,num2)
+  return num1/num2
+end
+
+class String
+  def numeric?
+    Float(self) != nil rescue false
+  end
+end
+
+operators = ["add","+","subtract","-","multiply","*", "divide","/"]
+# User imput examples
+# add 3 to 4
+# add 3 + 4
+# 3 + 4
+
+puts "Welcome to the calculator! How can I help you?"
+# input = gets.chomp
+# if input includes "add or +" find the numbers and do addition
+
+print "Would you like to add(+), subtract(-), multiply(*), divide(/) two numbers? "
+operator = gets.chomp
+
+# checks that operator exsists in the program
+while !operators.include?(operator)
+  puts "That is not an operation the calculator supports."
+  print "Please choose add(+), subtract(-), multiply(*), divide(/): "
+  operator = gets.chomp
+end
+
+# asks for the first number
+print "Please give me the first number: "
+num1 = gets.chomp
+
+while !num1.numeric?
+  print "That is not a number. Please enter a number: "
+  num1 = gets.chomp
+end
+
+num1 = num1.to_f
+
+# asks for the second number
+print "Please give me the second number: "
+num2 = gets.chomp
+
+while !num2.numeric?
+  print "That is not a number. Please enter a number: "
+  num2 = gets.chomp
+end
+
+num2 = num2.to_f
+
+if num2 == 0
+  puts "I'm sorry, but I can't divide by 0."
+  puts"Please run the program again to select a new equation."
+  return
+end
+
+case operator
+when "add", "+"
+  puts add(num1,num2)
+when "subtract", "-"
+  puts subtract(num1,num2)
+when "multiply", "*"
+  puts multiply(num1,num2)
+when "divide", "/"
+  puts divide(num1,num2)
+end
