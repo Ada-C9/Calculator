9ff52c7741c3ed25ee47c257bf21448b19d40c00
Kate Pond <kate@thepondsedge.com>
2018-02-07 09:34:19 -0800

    adds feature 'exponents' to program

--- calulator.rb
+++ calulator.rb
@@ -11,7 +11,11 @@ def multiply(num1,num2)
 end
 
 def divide(num1,num2)
-  return num1/num2
+  return num1 / num2
+end
+
+def exponent(num1,num2)
+  return num1 ** num2
 end
 
 class String
@@ -20,7 +24,10 @@ class String
   end
 end
 
-operators = ["add","+","subtract","-","multiply","*", "divide","/"]
+operators = [
+  "add","+","subtract","-","multiply","*", "divide","/", "power of",
+  "^", "exponent"
+]
 # User imput examples
 # add 3 to 4
 # add 3 + 4
@@ -30,14 +37,15 @@ puts "Welcome to the calculator! How can I help you?"
 # input = gets.chomp
 # if input includes "add or +" find the numbers and do addition
 
-print "Would you like to add(+), subtract(-), multiply(*), divide(/) two numbers? "
-operator = gets.chomp
+print "Would you like to add(+), subtract(-), multiply(*), divide(/), or "
+print "two numbers? You can also find the power of/exponent(^) of a number? "
+operator = gets.chomp.downcase
 
 # checks that operator exsists in the program
 while !operators.include?(operator)
   puts "That is not an operation the calculator supports."
-  print "Please choose add(+), subtract(-), multiply(*), divide(/): "
-  operator = gets.chomp
+  print "Please choose add(+), subtract(-), multiply(*), divide(/), or power of(^): "
+  operator = gets.chomp.downcase
 end
 
 # asks for the first number
@@ -77,4 +85,6 @@ when "multiply", "*"
   puts "#{num1} * #{num2} = #{multiply(num1,num2)}"
 when "divide", "/"
   puts "#{num1} / #{num2} = #{divide(num1,num2)}"
+when "power of","^", "exponent"
+  puts "#{num1} ^ #{num2} = #{exponent(num1,num2)}"
 end
