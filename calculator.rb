f704ddc7d7480584526614257d51c167147cf773
Erika Hughes <seattlesilencer@Gmail.com>
2018-02-08 01:34:20 -0800

    Update calculator.rb

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,13 @@
+# The program adds numbers with both add and +
+# The program subtracts numbers with both subtract and -
+# The program adds numbers with both multiply and *
+# The program adds numbers with both divide and /
+# The program handles divide when attempting to divide by zero.
+# The program handles erroneous input. For example the user might enter clown when asked to enter a number.
+# The program also needs to handle erroneous operators.
+
+
+
 def add(num1, num2)
   return num1 + num2
 end
@@ -16,41 +26,43 @@ end
 
 puts "Hello! Welcome to my Calculator App!"
 puts
-
 puts "Please enter a number!"
 begin
-  num1 = gets.chomp
+  num1 = gets.chomp.to_f
   num1 = Integer(num1)
 rescue
   puts "Please enter a numeric value!"
   retry
 end
 
-puts "Please enter an operator! (ex: add, - , divide, +, etc...):"
+puts "Please enter an operator!"
 operator = gets.chomp
-
-while true
-  break if operator == "add" || operator == "+" ||operator == "subtract" || operator =="minus" || operator == "-" || operator == "divide" || operator == "/" || operator == "mutliply" || operator == "times" || operator == "*"
-  puts "Not a valid operator!"
+until
+  ["add", "+", "subtract", "-", "divide", "/", "multiply", "*"].include?(operator)
+  puts "Please enter a valid operator!"
+  operator = gets.chomp
 end
-# fix loop
 
 puts "Please enter another number!"
 begin
-  num2 = gets.chomp
+  num2 = gets.chomp.to_f
   num2 = Integer(num2)
 rescue
   puts "Please enter a numeric value!"
   retry
 end
 
-if operator == "add" || operator == "+"
+case operator
+when "add", "+"
   puts add(num1, num2)
-elsif operator == "subtract" || operator =="minus" || operator == "-"
+when "subtract", "-"
   puts subtract(num1, num2)
-elsif operator == "divide" || operator == "/"
-  puts divide(num1, num2)
-else operator == "mutliply" || operator == "times" || operator == "*"
+when "multiply", "*"
   puts multiply(num1, num2)
-
+when "divide", "/"
+  if num2 == 0
+    puts "Cannot divide by 0"
+  else
+    puts divide(num1, num2)
+  end
 end
