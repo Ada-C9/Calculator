d836da7612dda1c84aee35c4c1f87a7d63df0884
Kiera Thomasson <kiera.thomasson@gmail.com>
2018-02-07 21:31:49 -0800

    Kiera Thomasson - Octos - Calculator

--- calculator.rb
+++ calculator.rb
@@ -22,46 +22,34 @@ def div(num1, num2)
   return num1 / num2
 end
 
-#Ask user for Data
+
+# Ask user for Data
 # Operation
 # two numbers
 # validate the data type from user
-puts "\nEnter the first number."
-def num
-  num = gets.chomp
-  until num =~ /^[\d]+?/
-    print 'Invalid number. Enter a valid number: '
-    num = gets.chomp
-    end
-  return num
-end
 
-num
-num1 = num.to_i
+puts "\nEnter the first number."
+num1 = gets.chomp
+until num1 =~ /\d/
+    print 'Invalid number. Enter a valid number: '
+    num1 = gets.chomp
+  end
 
 puts "\nChoose an Operation"
 operation = gets.chomp
 
-puts "\nEnter another number"
-num
+puts "\nEnter the second number."
+num2 = gets.chomp
+until num2 =~ /\d/
+    print 'Invalid number. Enter a valid number: '
+    num2 = gets.chomp
+  end
+
+num1 = num1.to_i
 num2 = num2.to_i
 
-# puts "\nEnter the second number."
-# num2 = gets.chomp.to_i
-# if num2 =~ /^[\d]+?/ || num2.to_i <= 0 && operation == "divide" || operation == "/"
-#   print 'Invalid number. Enter a valid number: '
-#   num2 = gets.chomp.to_i
-# end
 
-  # if num2 <= 0 && operation == "divide" || operation == "/"
-  #   puts "Invalid entry for a denominator. Please enter a number greater than 0."
-  #   operation = gets.chomp
-  # end
-#   while num1 == /^[\d]+?/ || num2 == /^[\d]+?/ do
-#     user_input
-#   end
-# end
-
+# handle the input from user
 
 case operation
   when "add", "+"
@@ -75,5 +63,3 @@ case operation
   else
     print "I am not quite sure what you want to do!"
 end
-
-# handle the input from user
