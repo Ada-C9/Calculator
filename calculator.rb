e70547fb96a9329c0477a4b431366d99fdf8cbfd
kseastman <kat.eastman@gmail.com>
2018-02-07 19:27:21 -0800

    Calculator-Update
    no optionals, just functionality

--- calculator.rb
+++ calculator.rb
@@ -1,26 +1,71 @@
-require 'set'
-operators = ["add", "subtract", "multiply", "divide", "modulo"]
+def add(num1, num2)
+  num1 + num2
+end
 
-def validate(string)
-  case string
-  when /\D/
-    # Check to see if operators were used
-    values = string.split(" ")
-    check = values.to_set.intersect?(operators.to_set)
-    if check == false
-      puts "Please use add, subtract, multiply, divide or modulo or the corresponding symbols"
-      string = gets.chomp
-    else
+def subtract(num1, num2)
+  num1 - num2
+end
 
-    end
-    # checks for mathmatical operators, and calculates outright
-  when /\p{Sm}/
-    result = eval((string))
-    
+def divide(num1, num2)
+  if num2 != 0
+    num1 / num2
   else
+    puts "What are you doing? You can't divide by zero!"
+    return nil
   end
 end
-puts "Enter maths: "
-input = gets.chomp
-# result = eval((input))
-validate(input)
+
+def multiply(num1, num2)
+  num1 * num2
+end
+
+def verify(input)
+  input = gets.chomp
+  case input 
+  when "0"
+    input.to_i
+  when /\d/
+    input.to_i
+  else
+    puts "Please enter a number: "
+    verify(input)
+  end
+end
+
+answer = "yes"
+until answer == "no"
+  puts "-" * 20
+  puts "Calculator"
+  
+  puts "Enter a number: "
+  num1 = verify(num1)
+  
+  puts "Enter a second number: "
+  num2 = verify(num2)
+  
+  puts "Input an operator. \n(for a list of operators press enter)"
+  operator = gets.chomp
+  case operator
+  when ""
+    puts "Available operators: +, -, *, / or add, subtract, divide, multiply"
+    puts "\nInput an operator"
+  when "+", "add"
+    puts "-" * 20
+    puts add(num1, num2)
+  when "-", "subtract"
+    puts "-" * 20
+    puts subtract(num1, num2)
+  when "/", "divide"
+    puts "-" * 20
+    puts divide(num1, num2)
+  when "*", "multiply"
+    puts "-" * 20
+    puts multiply(num1, num2)
+  else
+    "I'm sorry, I can't calculate that!"
+  end
+  
+  puts ""
+  puts "Would you like to calculate something else?"
+  answer = gets.chomp.downcase
+end  
