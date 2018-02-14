567917c2904acdd9d285b524eb61415c0ef01926
samanthaberk <15961601+samanthaberk@users.noreply.github.com>
2018-02-13 18:46:15 -0800

    Update calculator.rb
    Moved checkForDecimal method, condensed getUserNumber method, and changed if statement for dividing by 0 to a while loop to keep prompting user for a valid input

--- calculator.rb
+++ calculator.rb
@@ -1,54 +1,39 @@
 # If the result is a whole number, program should return an integer. Otherwise, it should return a float rounded to 2 decimal points.
-def checkForDecimal(result)
-  if result % 1 == 0
-    result = result.to_i
+def checkForDecimal(num)
+  if num % 1 == 0
+    num = num.to_i
   else
-    result = result.to_f.round(2)
+    num = num.to_f
   end
 end
 
 # Methods for each operator
 def add(num1, num2)
   result = num1 + num2
-  checkForDecimal(result)
 end
 
 def subtract(num1, num2)
   result = num1 - num2
-  checkForDecimal(result)
 end
 
 def multiply(num1, num2)
   result = num1 * num2
-  checkForDecimal(result)
 end
 
 def divide(num1, num2)
   result = num1 / num2
-  checkForDecimal(result)
 end
 
 def exponent(num1, num2)
   result = num1 ** num2
-  checkForDecimal(result)
 end
 
 def modulo(num1, num2)
   result = num1 % num2
-  checkForDecimal(result)
 end
 
 # Methods for getting user numbers. Proram should keep prompting user until they enter an integer.
-def getFirstUserNum
-  begin
-    return Float(gets.chomp)
-  rescue
-    puts "Please enter the number as an integer."
-    retry
-  end
-end
-
-def getSecondUserNum
+def getUserNumber
   begin
     return Float(gets.chomp)
   rescue
@@ -72,37 +57,39 @@ while continue != "q"
 
   # Ask user for two numbers
   puts "Please enter your first number: "
-  num1 = getFirstUserNum
+  num1 = getUserNumber
+  checkForDecimal(num1)
 
   puts "Please enter your second number: "
-  num2 = getSecondUserNum
+  num2 = getUserNumber
+  checkForDecimal(num2)
 
   # If user is dividing numbers, prompt them for different numbers if they attempt to divide something by 0
-  if (operator == "/" || operator == "divide") && num2 == 0
+  while (operator == "/" || operator == "divide") && num2 == 0
     puts "Sorry, you can't divide a number by 0. Please re-type the numbers you would like to divide: "
-    num1 = getFirstUserNum
-    num2 = getSecondUserNum
+    num1 = getUserNumber
+    num2 = getUserNumber
   end
 
   # Return the result
   case operator
   when "add", "+"
-    result = add(num1, num2)
+    result = add(num1, num2).round(2)
     puts "#{num1} + #{num2} = #{result}"
   when "subtract", "-"
-    result = subtract(num1, num2)
+    result = subtract(num1, num2).round(2)
     puts "#{num1} - #{num2} = #{result}"
   when "multiply", "*"
-    result = multiply(num1, num2)
+    result = multiply(num1, num2).round(2)
     puts "#{num1} * #{num2} = #{result}"
   when "divide", "/"
-    result = divide(num1, num2)
+    result = divide(num1, num2).round(2)
     puts "#{num1} / #{num2} = #{result}"
   when "exponent", "^"
-    result = exponent(num1, num2)
+    result = exponent(num1, num2).round(2)
     puts "#{num1}^#{num2} = #{result}"
   when "modulo", "%"
-    result = modulo(num1, num2)
+    result = modulo(num1, num2).round(2)
     puts "#{num1} % #{num2} = #{result}"
   end
 
